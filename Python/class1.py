c4bb152322acd39810da6a9c1648de82bd00376f
iWork <iwork@mengqingshundeMacBook-Pro.local>
2020-12-08 13:52:56 +0800

    ML class 1

---
+++ Python/class1.py
@@ -0,0 +1,17 @@
+#class
+class Dog():
+    
+    def __init__ (self, name, age):
+        self.name = name
+        self.age = age
+    
+    def sit(self):
+        print(self.name.title() + "is now sitting")
+
+    def roll_over(self):
+        print(self.name.title() + "rolled over!")
+
+
+dog = Dog("tim", 2)
+dog.sit
+dog.roll_over
