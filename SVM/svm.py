1fd7bcb6f7ee1e6425c64d7fb1e9e97b1e919b72
shunshun <shun_shun@139.com>
2021-01-07 17:45:58 +0800

    big change

--- SVM/svm.py
+++ SVM/svm.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 #%matplotlib inline
 # This line is only needed if you have a HiDPI display
-# %config InlineBackend.figure_format = 'retina'
+# %config InlineBackend.figure_format = 'retina' 
 
 from sklearn.datasets import make_blobs, make_circles, make_moons
 from sklearn.preprocessing import StandardScaler
@@ -35,3 +35,16 @@ def gaussian_kernel(x, y, sigma=1):
         result = np.exp(-(np.linalg.norm(x[:,np.newaxis]-y[:,np.newaxis],axis=2)**2)/2*sigma**2)
     return result
 
+def objective_function(alphas, target, kernel, X_train):
+
+    """Returns the SVM objective function based in the input model defined by
+    alphas: vector of Lagrange multipliers
+    target: vector of class labels (-1 or 1) for training data
+    kernel: kernel function
+    X_train: training data for model."""
+
+    return np.sum(alphas) - 0.5*np.sum((target[:,None]*target[None,:])*kernel(X_train, X_train)*(alphas[:,None]*alphas[None,:]))
+
+def decision_function(alphas, target, kernel, X_train, x_test, b):
+    """Applies the SVM decision function to the input feature vectors in `x_test`."""
+    return (alphas*target) @ kernel(X_train, x_test) + b
